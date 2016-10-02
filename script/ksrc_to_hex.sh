3fbcf526b5a3d2143941565bd65f8151840c5b32
mUogoro <muogoro@gmail.com>
2016-10-02 17:15:28 +0200

    Fix opencl kernel to hex for avoiding multiple inclusions error

--- script/ksrc_to_hex.sh
+++ script/ksrc_to_hex.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+
 kernels_src_dir=$1
 kernels_hex_dir=$2
 
 cd $kernels_src_dir
+
 for kernel_src in *.cl; do
-  xxd -i $kernel_src>$kernels_hex_dir/$kernel_src.inc
+  kernel_name=$(echo "$kernel_src"|cut -d . -f 1)
+  touch $kernels_hex_dir/$kernel_src.inc
+  echo "#ifndef ${kernel_name}_HPP"   > $kernels_hex_dir/$kernel_src.inc
+  echo "#define ${kernel_name}_HPP"   >> $kernels_hex_dir/$kernel_src.inc
+  xxd -i $kernel_src                      >> $kernels_hex_dir/$kernel_src.inc
+  echo "#endif // ${kernel_name}_HPP" >> $kernels_hex_dir/$kernel_src.inc
 done
